aed363d17093b1c0a1c05597f4eefee7d3d3bd52
ak811 <ali.khaleghi.r@gmail.com>
2022-08-01 18:36:38 +0000

    Add Constants.java Update AES.java: Code Refactoring

--- src/AES.java
+++ src/AES.java
@@ -19,84 +19,80 @@ public class AES {
 
     private static void menu() {
         while (true) {
-            System.out.println("Menu:\n" +
-                    "1- Encrypt a file \n" +
-                    "2- Decrypt a file\n" +
-                    "3- Exit\n");
+            System.out.println(Constants.MENU_MSG);
 
             Scanner input = new Scanner(System.in);
             int num = input.nextInt();
             input.nextLine();
+
+            if (num == 3) {
+                System.exit(Constants.EXIT_STATUS_CODE);
+                return;
+            }
+
+            System.out.println(num == 1 ? Constants.ENTER_FILE_PATH_FOR_ENCRYPTION_MSG : Constants.ENTER_FILE_PATH_FOR_DECRYPTION_MSG);
+            String filePath = input.nextLine();
+
+            System.out.println(Constants.ENTER_THE_KEY_MSG);
+            String key = input.nextLine();
+
+            System.out.println(Constants.ENTER_THE_DESTINATION_FILE_PATH_MSG);
+            String destinationFilePath = input.nextLine();
+
+            System.out.println(num == 1 ? Constants.ENCRYPTING_MSG : Constants.DECRYPTING_MSG);
+
             switch (num) {
                 case 1:
-                    System.out.println("Enter the file path you want to encrypt:");
-                    String filePath = input.nextLine();
-
-                    System.out.println("Enter the key:");
-                    String key = input.nextLine();
-
-                    System.out.println("Enter the destination file path:");
-                    String destinationFilePath = input.nextLine();
-
-                    System.out.println("\nEncrypting ...\n");
-
-                    try {
-                        Scanner scanner = new Scanner(new File(filePath));
-                        StringBuilder originalString = new StringBuilder();
-                        while (scanner.hasNextLine()) {
-                            String line = scanner.nextLine();
-                            originalString.append(line).append("\n");
-                        }
-                        String encryptedString = AES.encrypt(originalString.toString(), key);
-                        Formatter formatter = new Formatter(destinationFilePath);
-                        assert encryptedString != null;
-                        formatter.format(encryptedString);
-                        formatter.flush();
-                        formatter.close();
-
-                        System.out.println("The file encrypted.\n");
-                    } catch (Exception e) {
-                        System.out.println("incorrect file name entered.\n");
-                        break;
-                    }
+                    encryptFile(key, filePath, destinationFilePath);
                     break;
                 case 2:
-                    System.out.println("Enter the file path you want to decrypt:");
-                    filePath = input.nextLine();
-
-                    System.out.println("Enter the key:");
-                    key = input.nextLine();
-
-                    System.out.println("Enter the destination file path:");
-                    destinationFilePath = input.nextLine();
-
-                    System.out.println("\nDecrypting ...\n");
-
-                    try {
-                        Scanner scanner = new Scanner(new File(filePath));
-                        StringBuilder encryptedString = new StringBuilder();
-                        while (scanner.hasNextLine()) {
-                            String line = scanner.nextLine();
-                            encryptedString.append(line);
-                        }
-                        String decryptedString = AES.decrypt(encryptedString.toString(), key);
-                        Formatter formatter = new Formatter(destinationFilePath);
-                        assert decryptedString != null;
-                        formatter.format(decryptedString);
-                        formatter.flush();
-
-                        System.out.println("The file decrypted.\n");
-                    } catch (Exception e) {
-                        System.out.println("incorrect file name entered.\n");
-                        break;
-                    }
-                    break;
-                case 3:
-                    System.exit(0);
+                    decryptFile(key, filePath, destinationFilePath);
                     break;
                 default:
-                    System.out.println("Wrong key entered.\n");
+                    System.out.println(Constants.WRONG_INPUT_MSG);
             }
+        }
+    }
+
+    private static void encryptFile(String key, String filePath, String destinationFilePath) {
+        try {
+            Scanner scanner = new Scanner(new File(filePath));
+            StringBuilder originalString = new StringBuilder();
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                originalString.append(line).append(Constants.NEW_LINE);
+            }
+            String encryptedString = AES.encrypt(originalString.toString(), key);
+            Formatter formatter = new Formatter(destinationFilePath);
+            assert encryptedString != null;
+            formatter.format(encryptedString);
+            formatter.flush();
+            formatter.close();
+
+            System.out.println(Constants.FILE_ENCRYPTED_MSG);
+        } catch (Exception e) {
+            System.out.println(Constants.ENTER_FILE_PATH_FOR_DECRYPTION_MSG);
+        }
+    }
+
+    private static void decryptFile(String key, String filePath, String destinationFilePath) {
+        try {
+            Scanner scanner = new Scanner(new File(filePath));
+            StringBuilder encryptedString = new StringBuilder();
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                encryptedString.append(line);
+            }
+            String decryptedString = AES.decrypt(encryptedString.toString(), key);
+            Formatter formatter = new Formatter(destinationFilePath);
+            assert decryptedString != null;
+            formatter.format(decryptedString);
+            formatter.flush();
+            formatter.close();
+
+            System.out.println(Constants.FILE_DECRYPTED_MSG);
+        } catch (Exception e) {
+            System.out.println(Constants.INCORRECT_FILE_NAME_MSG);
         }
     }
 
@@ -104,10 +100,10 @@ public class AES {
         MessageDigest SHA;
         try {
             byte[] key = myKey.getBytes(StandardCharsets.UTF_8);
-            SHA = MessageDigest.getInstance("SHA-1");
+            SHA = MessageDigest.getInstance(Constants.MESSAGE_DIGEST_INSTANCE);
             key = SHA.digest(key);
-            key = Arrays.copyOf(key, 16);
-            secretKeySpec = new SecretKeySpec(key, "AES");
+            key = Arrays.copyOf(key, Constants.NEW_KEY_LENGTH);
+            secretKeySpec = new SecretKeySpec(key, Constants.AES_STR);
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
@@ -116,11 +112,11 @@ public class AES {
     public static String encrypt(String strToEncrypt, String secret) {
         try {
             setKey(secret);
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            Cipher cipher = Cipher.getInstance(Constants.CIPHER_INSTANCE_STR);
             cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
             return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
         } catch (Exception e) {
-            System.out.println("Error while encrypting: " + e.toString());
+            System.out.println(Constants.ERR_WHILE_ENCRYPTING + e.toString());
         }
         return null;
     }
@@ -128,11 +124,11 @@ public class AES {
     public static String decrypt(String strToDecrypt, String secret) {
         try {
             setKey(secret);
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            Cipher cipher = Cipher.getInstance(Constants.CIPHER_INSTANCE_STR);
             cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
             return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
         } catch (Exception e) {
-            System.out.println("Error while decrypting: " + e.toString());
+            System.out.println(Constants.ERR_WHILE_DECRYPTING + e.toString());
         }
         return null;
     }
